1a0f9706d3d987f37e5e315e16c07486ca944650
Cario Wix <cario@wix.com>
2023-01-23 13:25:38 +0200

    chore: new-comp template added children and comment (#3)

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -2,8 +2,13 @@ import { st, classes } from './new-component.st.css';
 
 export interface NewComponentProps {
     className?: string;
+    children?: React.ReactNode;
 }
 
-export const NewComponent = ({ className }: NewComponentProps) => {
-    return <div className={st(classes.root, className)}>NewComponent</div>;
+/**
+ * This component was generated using Codux's built-in Default new component template.
+ * For details on on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ */
+export const NewComponent = ({ className, children = 'NewComponent' }: NewComponentProps) => {
+    return <div className={st(classes.root, className)}>{children}</div>;
 };

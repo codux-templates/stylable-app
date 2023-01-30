fe4e0eecff14cc19a74fe172c7a71783330b35a3
Peter Shershov <peters@wix.com>
2023-01-30 14:06:39 +0200

    fix(codux): typo in new component template

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -7,7 +7,7 @@ export interface NewComponentProps {
 
 /**
  * This component was generated using Codux's built-in Default new component template.
- * For details on on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
  */
 export const NewComponent = ({ className, children = 'NewComponent' }: NewComponentProps) => {
     return <div className={st(classes.root, className)}>{children}</div>;

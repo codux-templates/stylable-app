dbf9ab0595bd2468b923a8a8c0c9adf1cc813c5e
cario <cario@wix.com>
2023-02-05 11:10:00 +0200

    chore: modify new-component template

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -9,6 +9,6 @@ export interface NewComponentProps {
  * This component was generated using Codux's built-in Default new component template.
  * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
  */
-export const NewComponent = ({ className, children = 'NewComponent' }: NewComponentProps) => {
-    return <div className={st(classes.root, className)}>{children}</div>;
+export const NewComponent = ({ className, children }: NewComponentProps) => {
+    return <div className={st(classes.root, className)}>NewComponent {children}</div>;
 };
